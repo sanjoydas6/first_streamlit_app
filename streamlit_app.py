ce9800a5777668bed602eb777dad8074a7ebfabc
sanjoydas6 <sanjoyd9@gmail.com>
2023-01-05 18:53:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,3 +12,4 @@ streamlit.dataframe(my_fruit_list)
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 # Display the table on the page.
 my_fruit_list = my_fruit_list.set_index('Fruit')
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list_index),['Avocado', 'Strawberries'])
