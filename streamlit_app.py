ac273bc92c4d4025b803a9d17ff0f2a8c0b16ef2
sanjoydas6 <sanjoyd9@gmail.com>
2023-01-05 18:33:20 +0530

    Update streamlit_app.py
    Added more Items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 streamlit.title('My Parents New healthy Dinner')
-streamlit.title('Breakfast Menu')
+streamlit.header('Breakfast Menu')
+streamlit.text('Luchi Alur Dom')
+streamlit.text('Ragi Parota and Roti')
+streamlit.text('Boiled Egg and Butter Toast')
