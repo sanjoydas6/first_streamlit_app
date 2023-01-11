95ac4b90d4b8d46cd9479a590c14212022fc3984
sanjoydas6 <sanjoyd9@gmail.com>
2023-01-11 23:01:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 import pandas
-#import requests
+import requests
 import snowflake.connector
 from urllib.error import URLError
 
@@ -20,7 +20,7 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit")
 streamlit.text(fruityvice_response)
 streamlit.header("Fruityvice Fruit Advice!")
 streamlit.text(fruityvice_response.json())
