616ad71326b9373b175c08ce0a48aa0bb1cc4750
sanjoydas6 <sanjoyd9@gmail.com>
2023-01-12 10:14:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit")
+fruityvice_response = requests.post("https://fruityvice.com/api/fruit")
 streamlit.text(fruityvice_response)
 
 streamlit.text(fruityvice_response.json())
