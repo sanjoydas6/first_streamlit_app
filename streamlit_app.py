fe3607d5da6dc2bcd9a129ec85415ec080d34603
sanjoydas6 <sanjoyd9@gmail.com>
2023-01-12 10:19:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
-fruityvice_response = requests.post("https://fruityvice.com/api/fruit")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit")
 streamlit.text(fruityvice_response)
 
 streamlit.text(fruityvice_response.json())
@@ -32,6 +32,7 @@ streamlit.text(fruityvice_response.json())
 #create a repeatable code block (called function)
 def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit" + this_fruit_choice)
+  streamlit.text('Thanks for Adding the Fruit')
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 #New Section to display fruityvice api response
